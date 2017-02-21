f85ccda6698104964fa3ca820814b48a27f66af4
Clément Garbay <clementgarbay@gmail.com>
2017-02-21 22:45:12 +0100

    Adding app.conf.js with personal firebase credentials (to remove in the future)

---
+++ www/js/app.conf.js
@@ -0,0 +1,16 @@
+(() => {
+  'use strict'
+
+  // TODO: remove this file from version control system !
+
+  angular
+    .module('whatsapp')
+    .constant('FirebaseConfig', {
+      apiKey: 'AIzaSyCTv54yvzsU52Aegyy0l-sMXiRbKTmwXhE',
+      authDomain: 'whatsapplike-f3034.firebaseapp.com',
+      databaseURL: 'https://whatsapplike-f3034.firebaseio.com',
+      storageBucket: 'whatsapplike-f3034.appspot.com',
+      messagingSenderId: '8574997754'
+    })
+
+})()
